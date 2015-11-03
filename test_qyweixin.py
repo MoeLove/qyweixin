98997560228c8da8caf1e5fb7428ce2697c24c06
ZhangJintao <tao12345666333@163.com>
2015-11-03 23:53:00 +0800

    add test

--- test_qyweixin.py
+++ test_qyweixin.py
@@ -10,8 +10,11 @@ except ImportError:
         raise
 
 import unittest
+import os
 
 import qyweixin
+
+TEST_TOKEN = 'F7BDI83twIdkvYoocoxB5DgXLiv69_H_OW_M758NQqxl3rL1uzSDJ6VrdEgwOr4WikApPVJgSqSEvwGyoi8bWw'
 
 
 class QyweixinTestCase(unittest.TestCase):
@@ -21,11 +24,33 @@ class QyweixinTestCase(unittest.TestCase):
 
         assert token is False
 
-    def test_push_text_msg(self):
+    def test_push_text_msg_wrong_token(self):
         push_msg = qyweixin.WeixinPush()
         result = push_msg.push_text_msg('text_token')
 
         assert result is False
+
+    def test_push_text_msg_correct_token(self):
+        push_msg = qyweixin.WeixinPush()
+        result = push_msg.push_text_msg(TEST_TOKEN, agentid=0,
+                                        content='test_msg')
+
+        assert result is True
+
+    def test_upload_file_and_push_msg(self):
+        if TEST_TOKEN:
+            os.system('echo "test upload file for qyweixin" > test_qyweixin.txt')
+            media_id = qyweixin.upload(TEST_TOKEN,
+                                      'test_qyweixin.txt',
+                                      'test_qyweixin.txt', 'file')
+
+            push_msg = qyweixin.WeixinPush()
+            result = push_msg.push_file_msg(TEST_TOKEN, agentid=0, media_id=media_id)
+
+            assert media_id is not False
+            assert result is True
+
+            os.system('rm test_qyweixin.txt')
 
 
 if __name__ == '__main__':
